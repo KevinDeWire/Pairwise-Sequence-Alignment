79bd8cd01835b95bac607a564767835525b3788f
Kevin DeWire <kevind4272@yahoo.com>
2019-10-03 19:18:24 -0400

    Generate the Optimal Alignment

--- src/Project1/PairwiseSequenceAlignment.java
+++ src/Project1/PairwiseSequenceAlignment.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Project1;
 
 /**
- *
- * @author kevin
+ * Kevin DeWire
+ * CS 723 Introduction to Bioinformatics
+ * Implementation of the Needleman Wunsch method of global alignment
  */
 public class PairwiseSequenceAlignment extends javax.swing.JFrame {
 
